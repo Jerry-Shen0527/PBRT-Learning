d6ece690645dad6842bb5ca0fc02691483449338
qwq <623180263@qq.com>
2021-11-10 13:46:00 +0800

    qwq

---
+++ PBRT/PBRT.cpp
@@ -0,0 +1,162 @@
+﻿// PBRT.cpp: 定义应用程序的入口点。
+//
+
+#include "rtweekend.h"
+
+#include "color.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#include "camera.h"
+#include "material.h"
+#include "moving_sphere.h"
+#include "bvh.h"
+#include <omp.h>
+#include <time.h>
+
+color ray_color(const ray& r, const hittable& world, const bvh_node& bvh, int depth) {
+    hit_record rec;
+    if (depth <= 0)
+        return color(0, 0, 0);
+    //if (world.hit(r, 0.001, infinity, rec)) {
+    if (bvh.hit(r, 0.001, infinity, rec))
+    {
+        ray scattered;
+        color attenuation;
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+            return attenuation * ray_color(scattered, world, bvh, depth - 1);
+        return color(0, 0, 0);
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+hittable_list random_scene() {
+    hittable_list world;
+
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
+
+    int n = 11;
+    for (int a = -n; a < n; a++) {
+        for (int b = -n; b < n; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    auto center2 = center + vec3(0, random_double(0, .5), 0);
+                    world.add(make_shared<moving_sphere>(
+                        center, center2, 0.0, 1.0, 0.2, sphere_material));
+                    world.setTime(0.0, 1.0);
+                }
+                else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+                else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
+hittable_list two_spheres() {
+    hittable_list objects;
+
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+
+    objects.add(make_shared<sphere>(point3(0, -10, 0), 10, make_shared<lambertian>(checker)));
+    objects.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
+
+    return objects;
+}
+int main() {
+
+    // Image
+
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+    int samples_per_pixel = 16;
+    const int max_depth = 50;
+
+    // World
+
+    hittable_list world;
+
+    point3 lookfrom;
+    point3 lookat;
+    auto vfov = 40.0;
+    auto aperture = 0.0;
+
+    switch (0) {
+    case 1:
+        world = random_scene();
+        lookfrom = point3(13, 2, 3);
+        lookat = point3(0, 0, 0);
+        vfov = 20.0;
+        aperture = 0.1;
+        break;
+
+    default:
+    case 2:
+        world = two_spheres();
+        lookfrom = point3(13, 2, 3);
+        lookat = point3(0, 0, 0);
+        vfov = 20.0;
+        break;
+    }
+    bvh_node bvh(world, world._time0, world._time1);
+    // Camera
+
+    vec3 vup(0, 1, 0);
+    auto dist_to_focus = 10.0;
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
+
+    // Render
+    clock_t start = clock();
+    printf("P3\n%d %d\n255\n", image_width, image_height);
+    for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+
+        for (int i = 0; i < image_width; ++i) {
+            color pixel_color(0, 0, 0);
+#pragma omp parallel for
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world,bvh, max_depth);
+            }
+            write_color( pixel_color, samples_per_pixel);
+        }
+    }
+    clock_t end = clock();
+    std::cerr << "\nSpend time:" << (end - start) / 1000 << "s. Done.\n";
+    
+}
---
+++ PBRT/hittable_list.h
@@ -0,0 +1,175 @@
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.h"
+#include "rtweekend.h"
+
+#include <memory>
+#include <vector>
+#include <algorithm>
+
+using std::shared_ptr;
+using std::make_shared;
+
+class hittable_list : public hittable {
+public:
+    hittable_list() {}
+    hittable_list(shared_ptr<hittable> object) { add(object); }
+
+    void clear() { objects.clear(); }
+    void add(shared_ptr<hittable> object) { objects.push_back(object); }
+    void setTime(double t0, double t1) { _time0 = t0; _time1 = t1; };
+
+    virtual bool hit(
+        const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+    virtual bool bounding_box(
+        double time0, double time1, aabb& output_box) const override;
+public:
+    std::vector<shared_ptr<hittable>> objects;
+    double _time0, _time1;
+};
+
+bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    hit_record temp_rec;
+    bool hit_anything = false;
+    auto closest_so_far = t_max;
+
+    for (const auto& object : objects) {
+        if (object->hit(r, t_min, closest_so_far, temp_rec)) {
+            hit_anything = true;
+            closest_so_far = temp_rec.t;
+            rec = temp_rec;
+        }
+    }
+    return hit_anything;
+}
+
+//list中所有元素的bounding box，若有元素无bounding box 返回false
+bool hittable_list::bounding_box(double time0, double time1, aabb& output_box) const {
+    if (objects.empty()) return false;
+
+    aabb temp_box;
+    bool first_box = true;
+
+    for (const auto& object : objects) {
+        if (!object->bounding_box(time0, time1, temp_box)) return false;
+        output_box = first_box ? temp_box : surrounding_box(output_box, temp_box);
+        first_box = false;
+    }
+
+    return true;
+}
+#endif
+
+#ifndef BVH_H
+#define BVH_H
+
+class bvh_node : public hittable {
+public:
+    bvh_node();
+
+    bvh_node(const hittable_list& list, double time0, double time1)
+        : bvh_node(list.objects, 0, list.objects.size(), time0, time1)
+    {}
+
+    bvh_node(
+        const std::vector<shared_ptr<hittable>>& src_objects,
+        size_t start, size_t end, double time0, double time1);
+
+    virtual bool hit(
+        const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+    virtual bool bounding_box(double time0, double time1, aabb& output_box) const override;
+
+public:
+    shared_ptr<hittable> left;
+    shared_ptr<hittable> right;
+    aabb box;
+};
+
+inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis) {
+    aabb box_a;
+    aabb box_b;
+
+    if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
+        std::cerr << "No bounding box in bvh_node constructor.\n";
+
+    return box_a.min().e[axis] < box_b.min().e[axis];
+}
+
+
+bool box_x_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 0);
+}
+
+bool box_y_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 1);
+}
+
+bool box_z_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 2);
+}
+
+
+bvh_node::bvh_node(
+    const std::vector<shared_ptr<hittable>>& src_objects,
+    size_t start, size_t end, double time0, double time1
+) {
+    auto objects = src_objects; // Create a modifiable array of the source scene objects
+
+    int axis = random_int(0, 2);
+    auto comparator = (axis == 0) ? box_x_compare
+        : (axis == 1) ? box_y_compare
+        : box_z_compare;
+
+    size_t object_span = end - start;
+
+    if (object_span == 1) {
+        left = right = objects[start];
+    }
+    else if (object_span == 2) {
+        if (comparator(objects[start], objects[start + 1])) {
+            left = objects[start];
+            right = objects[start + 1];
+        }
+        else {
+            left = objects[start + 1];
+            right = objects[start];
+        }
+    }
+    else {
+        std::sort(objects.begin() + start, objects.begin() + end, comparator);
+
+        auto mid = start + object_span / 2;
+        left = make_shared<bvh_node>(objects, start, mid, time0, time1);
+        right = make_shared<bvh_node>(objects, mid, end, time0, time1);
+    }
+
+    aabb box_left, box_right;
+
+    if (!left->bounding_box(time0, time1, box_left)
+        || !right->bounding_box(time0, time1, box_right)
+        )
+        std::cerr << "No bounding box in bvh_node constructor.\n";
+
+    box = surrounding_box(box_left, box_right);
+}
+
+bool bvh_node::bounding_box(double time0, double time1, aabb& output_box) const {
+    output_box = box;
+    return true;
+}
+
+bool bvh_node::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    if (!box.hit(r, t_min, t_max))
+        return false;
+
+    bool hit_left = left->hit(r, t_min, t_max, rec);
+    bool hit_right = right->hit(r, t_min, hit_left ? rec.t : t_max, rec);
+
+    return hit_left || hit_right;
+}
+
+
+#endif
